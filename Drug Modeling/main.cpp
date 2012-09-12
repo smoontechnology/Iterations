00333580f3dc52e1cdf822e5313f2ef36510a738
Stephen Moon <student@smoon2016.hotchkiss.org>
2012-09-12 16:55:34 -0400

    Initial Commit

---
+++ Drug Modeling/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Drug Modeling
+//
+//  Created by Stephen Moon on 9/12/12.
+//  Copyright (c) 2012 Stephen Moon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
